cd72436b14c4e5a9f9dde4a816f9cfe59cb6ad38
collins sanni <csanni52@gmail.com>
2023-03-05 06:45:35 +1100

    Change of backend url

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: "http://localhost:5000/",
-    // baseURL: process.env.REACT_APP_BACKEND_URL,
+    // baseURL: "http://localhost:5000/",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     // withCredentials: true,
     credentials:"include"    
 })
