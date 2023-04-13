18883564f1c86a19bcc2f097a284f39ebb726de9
sanite1 <86030930+sanite1@users.noreply.github.com>
2023-04-13 21:00:01 +1000

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "http://localhost:5000/",
-    baseURL: "https://smart-ana-backend-production.up.railway.app",
+    baseURL: "smart-ana-backend-production-7c8e.up.railway.app",
     // withCredentials: true,
     credentials:"include"    
 })
