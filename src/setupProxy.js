82783c06b08c9a6847373538c6d87b99cec3ed28
sanite1 <86030930+sanite1@users.noreply.github.com>
2023-04-13 21:05:22 +1000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = (app) => {
     app.use("/api", createProxyMiddleware({
-        target: "https://smart-ana-backend-production-7c8e.up.railway.app",
+        target: "https://smart-ana-backend-production-7c8e.up.railway.app/",
         changeOrigin: true
     }))
 }
